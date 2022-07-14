9be18e641056b79e276c2a473ac9303034093237
IliykhaDobrinskiy <bogdobrinskiy@gmail.com>
2022-07-14 16:49:51 +0300

    ADDED home2

---
+++ home2/TMShome2/TMShome2/Program.cs
@@ -0,0 +1,20 @@
+﻿static int fact(int n)
+{
+    if (n == 1 || n == 0)
+        return 1;
+    return n * fact(n - 1);
+}
+
+Console.Write("Enter number for factorial: ");
+
+if (int.TryParse(Console.ReadLine(), out var num))
+{
+    int fact_num = fact(num);
+    Console.WriteLine($"{num}! = {fact_num}");
+}
+else
+{
+    Console.WriteLine("You entered a strong number!");
+}
+
+Console.ReadKey();
